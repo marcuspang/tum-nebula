ea03cadd7ff1253f007f561f5841025536e4fbe7
Marcus Pang <marcuspangyuyang@gmail.com>
2023-11-23 00:14:00 +0100

    feat: add simple layout

---
+++ src/components/fleet-management.tsx
@@ -0,0 +1,222 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { SVGProps } from "react";
+import { ColourModeToggle } from "./colour-mode-toggle";
+
+export function FleetManagement() {
+  return (
+    <div key="1" className="flex flex-col h-screen">
+      <header className="flex items-center justify-between h-16 px-6 bg-transparent dark:bg-zinc-800/40 py-4 border-b">
+        <Link
+          className="flex items-center gap-2 font-semibold text-black dark:text-white"
+          href="#"
+        >
+          <IconDrone className="h-6 w-6" />
+          <span>Nebula</span>
+        </Link>
+        <nav className="flex items-center gap-6">
+          <ColourModeToggle />
+        </nav>
+      </header>
+      <div className="flex flex-1">
+        <aside className="w-64 h-full bg-zinc-100/40 dark:bg-zinc-800/40 p-6">
+          <h1 className="mb-4 text-xl font-semibold text-black dark:text-white">
+            Drone Fleet Management
+          </h1>
+          <nav className="flex flex-col gap-2 text-sm font-medium text-zinc-500 dark:text-zinc-400">
+            <Link href="#drone-status">Drone Status</Link>
+            <Link href="#flight-approval">Flight Approval</Link>
+            <Link href="#flight-logs">Flight Logs</Link>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6 bg-white dark:bg-black">
+          <h1 className="text-4xl font-bold text-zinc-800 dark:text-zinc-100">
+            Fleet 1 - ID: 12345
+          </h1>
+          <Card className="mt-4">
+            <CardHeader>
+              <CardTitle
+                id="drone-status"
+                className="text-zinc-800 dark:text-zinc-100"
+              >
+                Drone Status
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-zinc-800 dark:text-zinc-100">
+                Drone 1 (ID: 12345): Active
+              </p>
+              <p className="text-zinc-800 dark:text-zinc-100">
+                Drone 2 (ID: 67890): Inactive
+              </p>
+              <p className="text-zinc-800 dark:text-zinc-100">
+                Drone 3 (ID: 11223): Active
+              </p>
+            </CardContent>
+          </Card>
+          <Card className="mt-4">
+            <CardHeader>
+              <CardTitle
+                id="flight-approval"
+                className="text-zinc-800 dark:text-zinc-100"
+              >
+                Flight Approval
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Table className="text-zinc-800 dark:text-zinc-100">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Drone</TableHead>
+                    <TableHead>ATC report and acknowledgment</TableHead>
+                    <TableHead>
+                      Ground traffic authority report and acknowledgment
+                    </TableHead>
+                    <TableHead>
+                      The public emergency response department is acknowledged
+                    </TableHead>
+                    <TableHead>
+                      On-site environments are inspected to be suitable by
+                      flight operation team
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Drone 1</TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone1-cat1"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone1-cat2"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone1-cat3"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone1-cat4"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Drone 2</TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone2-cat1"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone2-cat2"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone2-cat3"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone2-cat4"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Drone 3</TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone3-cat1"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone3-cat2"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone3-cat3"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        id="drone3-cat4"
+                        className="border-black dark:border-white"
+                      />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+          <Card className="mt-4">
+            <CardHeader>
+              <CardTitle
+                id="flight-logs"
+                className="text-zinc-800 dark:text-zinc-100"
+              >
+                Flight Logs
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="bg-black text-green-400 font-mono p-4">
+              <p>[2023-11-22 10:00:00] Drone 1: Take off</p>
+              <p>[2023-11-22 10:15:00] Drone 1: Reached waypoint 1</p>
+              <p>[2023-11-22 10:30:00] Drone 1: Reached waypoint 2</p>
+              <p>[2023-11-22 10:45:00] Drone 1: Landing</p>
+              <p>[2023-11-22 11:00:00] Drone 2: Take off</p>
+              <p>[2023-11-22 11:15:00] Drone 2: Reached waypoint 1</p>
+              <p>[2023-11-22 11:30:00] Drone 2: Reached waypoint 2</p>
+              <p>[2023-11-22 11:45:00] Drone 2: Landing</p>
+            </CardContent>
+          </Card>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+function IconDrone(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M12 22a7 7 0 0 0 7-7c0-2-1-3.9-3-5.5s-3.5-4-4-6.5c-.5 2.5-2 4.9-4 6.5C6 11.1 5 13 5 15a7 7 0 0 0 7 7z" />
+    </svg>
+  );
+}
